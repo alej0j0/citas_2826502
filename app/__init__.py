f0fb51de1487211110ded901f8537b812b9ae028
Aprendiz <Aprendiz@BOGDFPCGMFN4138.FormacionSena.RED>
2024-02-20 17:32:02 -0500

    Migraciones

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+##importar config
+from .config import Config
+ 
+##crear objeto de aplicacion
+app = Flask(__name__)
+
+##configurar el objeto flask con el config
+app.config.from_object(Config)
+
+##crar objeto SQLAlchemy
+db = SQLAlchemy(app)
+
+##objeto para las migraciones
+migrate = Migrate(app , db)
+
+##importar modelos
+from .models import Medico
+
+##ejecutar el objeto
+
+if __name__ == '__main__':
+    app.run()
